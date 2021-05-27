c432b59fb83b783aed8a338d74cf66568e6f315b
Gery Akbar Fauzi <geryakbarfauzi@gmail.com>
2021-05-27 08:48:17 +0700

    - Menambahkan tampilan filter laporan kesehatan

--- app/src/main/java/unikom/gery/damang/activities/DailyConditionActivity.java
+++ app/src/main/java/unikom/gery/damang/activities/DailyConditionActivity.java
@@ -1,12 +1,18 @@
 package unikom.gery.damang.activities;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,13 +25,14 @@ import unikom.gery.damang.model.HeartRate;
 import unikom.gery.damang.sqlite.dml.HeartRateHelper;
 import unikom.gery.damang.util.SharedPreference;
 
-public class DailyConditionActivity extends AppCompatActivity {
+public class DailyConditionActivity extends AppCompatActivity implements View.OnClickListener {
 
     private HeartRateHelper heartRateHelper;
     private ArrayList<HeartRate> arrayList;
     private RecyclerView rvDailyCondition;
     private DailyConditionAdapter dailyConditionAdapter;
     private SharedPreference sharedPreference;
+    private ImageView btnBack, btnFilter;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -41,6 +48,8 @@ public class DailyConditionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_daily_condition);
         heartRateHelper = HeartRateHelper.getInstance(getApplicationContext());
         sharedPreference = new SharedPreference(getApplicationContext());
+        btnBack = findViewById(R.id.btnBack);
+        btnFilter = findViewById(R.id.btnFilter);
         //RecyclerView Binding
         rvDailyCondition = findViewById(R.id.rvDailyCondition);
         rvDailyCondition.setHasFixedSize(true);
@@ -48,5 +57,44 @@ public class DailyConditionActivity extends AppCompatActivity {
         arrayList = heartRateHelper.getDailyCondition(sharedPreference.getUser().getEmail());
         dailyConditionAdapter = new DailyConditionAdapter(arrayList, getApplicationContext());
         rvDailyCondition.setAdapter(dailyConditionAdapter);
+        //
+        btnFilter.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+    }
+
+    private void showAlertDialog() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pilih tipe laporan");
+        final View layout = getLayoutInflater().inflate(R.layout.custom_alert_dialog_laporan_kesehatan, null);
+        builder.setView(layout);
+
+        final RadioGroup radioGroup = layout.findViewById(R.id.btnGroup);
+        builder.setPositiveButton("Terapkan", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                int selectedID = radioGroup.getCheckedRadioButtonId();
+                RadioButton radioButton = layout.findViewById(selectedID);
+                Toast.makeText(getApplicationContext(), radioButton.getText(), Toast.LENGTH_SHORT).show();
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnBack)
+            finish();
+        else if (view == btnFilter) {
+            showAlertDialog();
+        }
     }
 }
